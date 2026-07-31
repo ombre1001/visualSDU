f641ee5f09ccf2ccca17fdf4e3feaad501efc0e4
Sentimental <CarolineFloyd3885@gmail.com>
2026-07-31 16:37:15 +0800

    build: 初始化并构建Maven多模块项目，添加SpringBoot启动类

---
+++ vsdu-boot/src/main/java/cn/sduonline/VisualSduApplication.java
@@ -0,0 +1,13 @@
+package cn.sduonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualSduApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VisualSduApplication.class, args);
+    }
+
+}
